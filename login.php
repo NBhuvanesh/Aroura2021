973c57c25dc407554d3b52f5b3ac40af2471a6cd
NBhuvanesh <76568488+NBhuvanesh@users.noreply.github.com>
2020-12-25 19:51:19 -0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,10 +1,12 @@
 <?php
-$db = 'database.accdb';
-if(!file_exists($db))
-{
-    die('could not find database');
-}
-else
-{
-    echo('database success');
-}
+    $db = 'database.accdb';
+    if(!file_exists($db))
+    {
+        die('could not find database');
+    }
+    else
+    {
+        echo('database success');
+    }
+    $db = new PDO("odbc:DRIVER=(Microsoft Access Driver (*.mdb)}; DBQ=$db; Uid=; Pwd=;");
+?>
