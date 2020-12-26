46ea3fc527857dc9038490f2b89969507e751fa4
NBhuvanesh <76568488+NBhuvanesh@users.noreply.github.com>
2020-12-25 19:44:06 -0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,7 +1,10 @@
 <?php
-try{
-    $myPDO = new PDO("pgsql:host=ec2-52-5-176-53.compute-1.amazonaws.com;dbname=dbv43ft67o6q8l","gwlqnlismchdjn","a6e79eaab9db81e59dfeddad1556b6ec93de2b722e273998cffa291f3afb6f90");
-    echo "Conected Successfully";
-}catch(PDOException $e){
-    echo $e->getMessage();
+$db = 'database.accdb';
+if(!file_exists($db))
+{
+    die('could not find database');
+}
+else
+{
+    echo('database success');
 }
